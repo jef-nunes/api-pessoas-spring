e80b0ec1c0b5d201d7abec21315dac3a23e24222
Jeferson <159483664+jef-nunes@users.noreply.github.com>
2025-05-28 16:07:09 -0300

    Create Repositorio.java

---
+++ api/repositorio/Repositorio.java
@@ -0,0 +1,11 @@
+package br.com.exemplo.api.repositorio;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.exemplo.api.modelo.Pessoa;
+
+@Repository
+public interface Repositorio extends CrudRepository<Pessoa, Integer>{
+    
+}
