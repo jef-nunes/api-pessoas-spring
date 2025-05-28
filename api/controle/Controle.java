fa2cb66848be9f7b74c909b17170bc894eb1acb1
Jeferson <159483664+jef-nunes@users.noreply.github.com>
2025-05-28 16:17:10 -0300

    Update Controle.java

--- api/controle/Controle.java
+++ api/controle/Controle.java
@@ -1,10 +1,12 @@
 package br.com.exemplo.api.controle;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.exemplo.api.modelo.Pessoa;
+import br.com.exemplo.api.repositorio.Repositorio;
 
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +15,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 public class Controle {
     
+    @Autowired
+    private Repositorio acao;
+
+    @PostMapping("/pessoas")
+    public Pessoa cadastrar(@RequestBody Pessoa obj){
+        return acao.save(obj);
+    }
+
     @GetMapping("")
     public String mensagem(){
         return "Olá Mundo";
