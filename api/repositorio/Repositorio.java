4883ea68927735a4e2dac2cb9b4a8cb12ebd7593
Jeferson <159483664+jef-nunes@users.noreply.github.com>
2025-05-28 16:26:32 -0300

    Update Repositorio.java

--- api/repositorio/Repositorio.java
+++ api/repositorio/Repositorio.java
@@ -1,5 +1,7 @@
 package br.com.exemplo.api.repositorio;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import br.com.exemplo.api.modelo.Pessoa;
 
 @Repository
 public interface Repositorio extends CrudRepository<Pessoa, Integer>{
-    
+    List<Pessoa> findAll();
 }
