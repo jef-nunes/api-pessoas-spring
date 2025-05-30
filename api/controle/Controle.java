2b6753131f30402dcd8a49d4a3e5f795ea5421af
Jeferson <159483664+jef-nunes@users.noreply.github.com>
2025-05-30 15:48:15 -0300

    Update Controle.java

--- api/controle/Controle.java
+++ api/controle/Controle.java
@@ -3,16 +3,18 @@ package br.com.exemplo.api.controle;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.exemplo.api.modelo.Pessoa;
 import br.com.exemplo.api.repositorio.Repositorio;
 
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 public class Controle {
@@ -28,6 +30,22 @@ public class Controle {
     @GetMapping("/api")
     public List<Pessoa> selecionar(){
         return acao.findAll();
+    }
+
+    @GetMapping("/api/{codigo}")
+    public Pessoa selecionarPeloCodigo(@PathVariable int codigo){
+        return acao.findByCodigo(codigo);
+    }
+    
+    @PutMapping("/api")
+    public Pessoa editar(@RequestBody Pessoa obj){
+        return acao.save(obj);
+    }
+    
+    @DeleteMapping("/api/{codigo}")
+    public void remover(@PathVariable int codigo){
+        Pessoa obj = selecionarPeloCodigo(codigo);
+        acao.delete(obj);
     }
 
     @GetMapping("")
